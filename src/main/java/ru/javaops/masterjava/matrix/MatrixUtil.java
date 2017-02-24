77c0e4df5bb463805488167fca7d5679e9d525b7
konstantin <jakimenko.k@gmail.com>
2017-02-24 22:31:46 +0300

    My homework 1

--- src/main/java/ru/javaops/masterjava/matrix/MatrixUtil.java
+++ src/main/java/ru/javaops/masterjava/matrix/MatrixUtil.java
@@ -1,8 +1,9 @@
 package ru.javaops.masterjava.matrix;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
+import java.util.concurrent.*;
 
 /**
  * gkislin
@@ -15,7 +16,60 @@ public class MatrixUtil {
         final int matrixSize = matrixA.length;
         final int[][] matrixC = new int[matrixSize][matrixSize];
 
-        return matrixC;
+        final CompletionService<Result> completionService = new ExecutorCompletionService<>(executor);
+
+        int[][] BT = new int[matrixSize][matrixSize];
+        for (int i = 0; i < matrixSize; i++) {
+            for (int j = 0; j < matrixSize; j++) {
+                BT[j][i] = matrixB[i][j];
+            }
+        }
+
+        List<Future<Result>> futures = new ArrayList<>();
+        for (int i = 0; i < matrixSize ; i++) {
+            final int i_final = i;
+            futures.add(completionService.submit(() -> {
+                int[] res = new int[matrixSize];
+                for (int j = 0; j < matrixSize; j++) {
+                    int sum = 0;
+                    for (int k = 0; k < matrixSize; k++) {
+                        sum += matrixA[i_final][k] * BT[j][k];
+                    }
+                    res[j] = sum;
+                }
+                return new Result(i_final, res);
+            }));
+        }
+
+        return new Callable<int[][]>() {
+            @Override
+            public int[][] call() {
+                while (!futures.isEmpty()) {
+                    try {
+                        Future<Result> future = completionService.poll(10, TimeUnit.SECONDS);
+                        if (future == null) {
+                            new Exception("Timeout exception");
+                        }
+                        futures.remove(future);
+                        Result res = future.get();
+                        matrixC[res.i] = res.result;
+                    } catch (InterruptedException | ExecutionException e) {
+                        e.printStackTrace();
+                    }
+                }
+                return matrixC;
+            }
+        }.call();
+    }
+
+    public static class Result {
+        public final int i;
+        public final int[] result;
+
+        public Result(int i, int[] result) {
+            this.i = i;
+            this.result = result;
+        }
     }
 
     // TODO optimize by https://habrahabr.ru/post/114797/
@@ -23,15 +77,23 @@ public class MatrixUtil {
         final int matrixSize = matrixA.length;
         final int[][] matrixC = new int[matrixSize][matrixSize];
 
-        for (int i = 0; i < matrixSize; i++) {
-            for (int j = 0; j < matrixSize; j++) {
-                int sum = 0;
+        int[] thatColumn = new int[matrixSize];
+        try {
+            for (int j = 0; ; j++) {
                 for (int k = 0; k < matrixSize; k++) {
-                    sum += matrixA[i][k] * matrixB[k][j];
+                    thatColumn[k] = matrixB[k][j];
                 }
-                matrixC[i][j] = sum;
+
+                for (int i = 0; i < matrixSize; i++) {
+                    int[] thatRow = matrixA[i];
+                    int summand = 0;
+                    for (int k = 0; k < matrixSize; k++) {
+                        summand += thatRow[k]*thatColumn[k];
+                    }
+                    matrixC[i][j] = summand;
+                }
             }
-        }
+        } catch (IndexOutOfBoundsException ignored) {}
         return matrixC;
     }
 
